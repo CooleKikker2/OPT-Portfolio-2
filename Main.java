53d309a29fd91371ee041b6241745f77eaf64f9d
Coen Vink <pctcoen@gmail.com>
2024-04-07 13:32:29 +0200

    Added home page

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+class Page
+{
+    public void menu(Scanner scanner)
+    {
+        System.out.println("Welkom bij je orgelboekencollectie");
+        System.out.println("Maak een keuze om verder te gaan:");
+        System.out.println("1. Boek toevoegen aan collectie");
+        int keuze = scanner.nextInt();
+        switch (keuze)
+        {
+            case 1:
+                this.voegBoekToe(scanner);
+                break;
+        }
+    }
+
+    public void voegBoekToe(Scanner scanner)
+    {
+
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Page page = new Page();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        page.menu(scanner);
     }
 }
