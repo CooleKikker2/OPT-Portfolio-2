6717dbe0f0125c2c022319eb34ee59b3b6211ca0
Coen Vink <pctcoen@gmail.com>
2024-04-07 21:22:37 +0200

    Added user inputs for books and songs

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ class Page
         System.out.println("Maak een keuze om verder te gaan:");
         System.out.println("1. Boek toevoegen aan collectie");
         int keuze = scanner.nextInt();
+        scanner.nextLine();
         switch (keuze)
         {
             case 1:
@@ -18,7 +19,20 @@ class Page
 
     public void voegBoekToe(Scanner scanner)
     {
+        System.out.println("Wat is de naam van het boekje?");
+        String boekje = scanner.nextLine();
+        System.out.println("Welke liederen staan er in het boekje? Typ 'stop' al je alle liederen hebt toegevoegd");
+        while(true)
+        {
+            String lied = scanner.nextLine();
+            if(lied.equals("stop"))
+            {
+                break;
+            }
 
+            System.out.printf("Lied %s toegevoegd aan %s \n", lied, boekje);
+            System.out.println("Voeg nog een lied toe aan het boekje of typ 'stop'");
+        }
     }
 }
 public class Main {
